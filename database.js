c766f1941c2462dc3c0d645d3e951289957da34f
Christian Dj <christiandj456@gmail.com>
2025-04-25 10:13:38 -0500

    Cambio en el path de las base de datos

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Conexión a la base de datos SQLite corregida
-const dbPath = path.resolve(__dirname, './db/datos_pacientes_con_meds.db');
+const dbPath = path.resolve(__dirname, './db/DataPrueba_pacientes.db');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('❌ Error al conectar con SQLite:', err.message);
